264d340d6d464138cb2da31c1033de83f444eb76
Hernan Guzman <hernandgr@gmail.com>
2019-12-14 15:22:52 -0500

    Add files via upload

---
+++ src/app/mesages/mesages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-mesages',
+  templateUrl: './mesages.component.html',
+  styleUrls: ['./mesages.component.css']
+})
+export class MesagesComponent implements OnInit {
+
+  constructor(public messageService: MessageService) {}
+
+  ngOnInit() {
+  }
+
+}
